f2e0444193d0e0acdf36d7f9fa1855f2cae60e57
asqua1991 <aquariusstar1991@gmail.com>
2021-05-07 13:56:59 -0700

    feat(global context): is_loading state functionality

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -6,6 +6,7 @@ const initialState = {
   products: [],
   cart: [],
   product: undefined,
+  is_loading: false,
   getProducts: () => {},
   getSingleProduct: () => {},
   addToCart: () => {},
@@ -31,11 +32,12 @@ const appReducer = (state: any, action: any) => {
       return { ...state, products: action.payload };
     case 'GET_SINGLE_PRODUCT':
       // when case matches, bind the payload to the product property in state
-      return { ...state, product: action.payload };
+      return { ...state, product: action.payload, is_loading: false };
     case 'ADD_TO_CART':
       console.log('what is payload?', action.payload);
       return { ...state, cart: [...state.cart, action.payload] };
-
+    case 'SET_LOADING':
+      return { ...state, is_loading: action.payload };
     default:
       return state;
   }
@@ -61,6 +63,7 @@ export const GlobalProvider: React.FC = ({ children }) => {
   };
 
   const getSingleProduct = async (productId: number) => {
+    dispatch({ type: 'SET_LOADING', payload: true });
     try {
       let { data } = await instance.get(`/products/${productId}`);
       console.log(data);
@@ -81,6 +84,7 @@ export const GlobalProvider: React.FC = ({ children }) => {
         products: state.products,
         cart: state.cart,
         product: state.product,
+        is_loading: state.is_loading,
         getProducts,
         getSingleProduct,
         addToCart,
